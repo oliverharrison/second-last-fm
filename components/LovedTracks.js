988aa8cf84f0c8d02aa7644a521b32c5cd667fea
Oliver Harrison <oliver@positivebias.com>
2021-11-01 11:33:51 -0400

    Add styles to LovedTracks component

--- components/LovedTracks.js
+++ components/LovedTracks.js
@@ -1,5 +1,6 @@
 import { getLastFmData } from "../pages/api/getLastFmData";
 import { useEffect, useState } from "react";
+import styles from "../styles/LovedTracks.module.scss";
 
 const LovedTracks = () => {
   const [lovedTracks, setLovedTracks] = useState({ track: [] });
@@ -13,15 +14,18 @@ const LovedTracks = () => {
         console.log("rejected", err.message);
       });
   }, []);
+
   return (
-    <div className="loved-tracks">
+    <div className={styles.lovedtracks}>
       <h1>Loved Tracks</h1>
       <ul>
         {lovedTracks.track.map((track) => {
           return (
             <li key={track.mbid}>
-              {track.name} <br />
-              {track.artist.name}
+              <a href={track.url}>
+                {track.name} <br />
+                {track.artist.name}
+              </a>
             </li>
           );
         })}
